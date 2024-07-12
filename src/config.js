34a40d684cd97176c91d0eed7fa228946f716158
Abdykali Azat <azat.abdykalli@gmail.com>
2024-07-12 17:55:16 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://vk-grand-cargo.kz:3001',
+    
+    nameCargo: 'VK-GRAND-CARGO'
+  };
+  
+  
+export default config;
+  
